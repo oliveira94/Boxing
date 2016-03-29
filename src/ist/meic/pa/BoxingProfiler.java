cfff44bb19326b2e9500f991e04c68b9604b62d9
oliveira94 <arturoliveira@tecnico.ulisboa.pt>
2016-03-29 11:56:34 +0100

    loading... part3

--- src/ist/meic/pa/BoxingProfiler.java
+++ src/ist/meic/pa/BoxingProfiler.java
@@ -23,36 +23,40 @@ public class BoxingProfiler {
     public static void main(String[] args) throws CannotCompileException {
 
         try {
-            //create the ClassPool
             ClassPool pool = ClassPool.getDefault();
 
             CtClass cc = pool.get("ist.meic.pa.SumInts");
 
+            //to thread every method
             for (CtMethod ctMethod : cc.getDeclaredMethods()){
-                //valueof boxing
-                //...value unboxing
                 ctMethod.instrument(
                         new ExprEditor() {
                             public void edit(MethodCall m) throws CannotCompileException
                             {
+                                //conditon to detect boxing because when exist boxing the method valueOf
+                                //is always called
                                 if(m.getMethodName().equals("valueOf"))
                                 {
+                                    //if the method is main
                                     if(ctMethod.getName().equals("main")){
-                                        System.out.print(ctMethod.getLongName());
+                                        System.err.print(ctMethod.getLongName());
 
-                                        System.out.print(" boxed 1 ");
+                                        System.err.print(" boxed 1 ");
                                         try {
-                                            System.out.println(m.getMethod().getReturnType().getName());
+                                            //return the java language type of the returned value
+                                            System.err.println(m.getMethod().getReturnType().getName());
                                         } catch (NotFoundException e) {
                                             e.printStackTrace();
                                         }
                                     } else try {
+                                        //if is another method we need to compare the type of the parameter and
+                                        //the type of returned value, and if is, is a boxing, if not, isn't a boxing
                                         if(ctMethod.getLongName().toLowerCase().endsWith(m.getMethod().getReturnType().getName()+")")){
-                                            System.out.print(ctMethod.getLongName());
+                                            System.err.print(ctMethod.getLongName());
 
-                                            System.out.print(" boxed 1 ");
+                                            System.err.print(" boxed 1 ");
                                             try {
-                                                System.out.println(m.getMethod().getReturnType().getName());
+                                                System.err.println(m.getMethod().getReturnType().getName());
                                             } catch (NotFoundException e) {
                                                 e.printStackTrace();
                                             }
@@ -61,24 +65,30 @@ public class BoxingProfiler {
                                         e.printStackTrace();
                                     }
                                 }
+                                //conditon to detect unboxing because when exist unboxing the method like intValue
+                                //or longValue, so its always called a method that ends with Value
                                 else if(m.getMethodName().endsWith("Value"))
                                 {
                                     if(ctMethod.getName().equals("main")){
-                                        System.out.print(ctMethod.getLongName());
+                                        System.err.print(ctMethod.getLongName());
 
-                                        System.out.print(" unboxed 1 ");
+                                        System.err.print(" unboxed 1 ");
                                         try {
-                                            System.out.println(m.getMethod().getReturnType().getName().getClass());
+                                            String type_returned = m.getMethod().getReturnType().getName();
+                                            //called the method getType to get the java language type that its returned
+                                            getType(type_returned);
+
                                         } catch (NotFoundException e) {
                                             e.printStackTrace();
                                         }
                                     } else try {
                                         if(ctMethod.getLongName().toLowerCase().endsWith(m.getMethod().getReturnType().getName()+")")){
-                                            System.out.print(ctMethod.getLongName());
+                                            System.err.print(ctMethod.getLongName());
 
-                                            System.out.print(" unboxed 1 ");
+                                            System.err.print(" unboxed 1 ");
                                             try {
-                                                System.out.println(m.getMethod().getReturnType().getName().getClass());
+                                                String type_returned = m.getMethod().getReturnType().getName();
+                                                getType(type_returned);
                                             } catch (NotFoundException e) {
                                                 e.printStackTrace();
                                             }
@@ -92,6 +102,28 @@ public class BoxingProfiler {
             }
         }  catch (IllegalArgumentException | NotFoundException e) {
             e.printStackTrace();
+        }
+    }
+
+    //Method responsable for print a java language type based on a primitive type
+    public static void getType(String type){
+        switch (type){
+            case "int": System.err.println("java.lang.Integer");
+                break;
+            case "long": System.err.println("java.lang.Long");
+                break;
+            case "char": System.err.println("java.lang.Character");
+                break;
+            case "byte": System.err.println("java.lang.Byte");
+                break;
+            case "short": System.err.println("java.lang.Short");
+                break;
+            case "double": System.err.println("java.lang.Double");
+                break;
+            case "float": System.err.println("java.lang.Float");
+                break;
+            case "boolean": System.err.println("java.lang.Boolean");
+                break;
         }
     }
 }
